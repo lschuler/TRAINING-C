0085d95f91214cd05a7ea954f51edd3d55dae8d7
lschuler <ludovic.schuler@live.fr>
2018-11-15 02:16:08 -0800

    add new files

---
+++ BOUCLES/while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int nombreEntre;
+		
+		nombreEntre = 0;
+
+	while (nombreEntre != 47)
+{
+	printf("Tapez le nombre 47 !");
+	scanf("%d", &nombreEntre);
+
+}
+	return 0;
+}
---
+++ BOUCLES/whilecompteur.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int compteur;
+	
+	compteur = 20;
+do
+{
+	printf("je suis un zero\n");
+	compteur++;
+}while(compteur <10);
+
+	return 0;
+}
